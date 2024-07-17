8bdd6318d29cfecb897cd9c4f49076cf6f96314c
DasBlackfur <64478051+DasBlackfur@users.noreply.github.com>
2024-07-17 09:42:45 +0200

    Initial commit

---
+++ src/main/java/me/blackfur/pm/ProjectMachines.java
@@ -0,0 +1,10 @@
+package me.blackfur.pm;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ProjectMachines implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/blackfur/pm/client/ProjectMachinesClient.java
@@ -0,0 +1,10 @@
+package me.blackfur.pm.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectMachinesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
